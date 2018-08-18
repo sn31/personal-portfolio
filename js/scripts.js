aaa444ee93879f72abbe019fb143bba3e24a97ba
SkyeN <skye@dames.es>
2018-08-18 15:10:02 -0700

    style splash screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#myCarousel').carousel({
         interval: 3000,
         cycle: true
-      }); 
-      $('.enter_link').click(function() { 
-        $(this).parent().fadeOut(500);
- });
+    });
+    $('.enter_link').click(function () {
+        $("#splashscreen").fadeOut(500);
+    });
 })
