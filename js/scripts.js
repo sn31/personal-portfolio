9a1fe6fc847c7b531a3e44a01f8c6ab14753d1d1
SkyeN <skye@dames.es>
2018-08-20 21:10:52 -0700

    clean up HTML structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,42 @@
 $(document).ready(function () {
-    $('#myCarousel').carousel({
-        interval: 3000,
-        cycle: true
-    });
+   
     $('.enter_link').click(function () {
         $("#splashscreen").fadeOut(500);
     });
 
     // Wrap every letter in a span
-$('.ml11 .letters').each(function(){
-    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-  });
-  
-  anime.timeline({loop: true})
-    .add({
-      targets: '.ml11 .line',
-      scaleY: [0,1],
-      opacity: [0.5,1],
-      easing: "easeOutExpo",
-      duration: 1200
-    })
-    .add({
-      targets: '.ml11 .line',
-      translateX: [0,$(".ml11 .letters").width()],
-      easing: "easeOutExpo",
-      duration: 600,
-      delay: 100
-    }).add({
-      targets: '.ml11 .letter',
-      opacity: [0,1],
-      easing: "easeOutExpo",
-      duration: 1200,
-      offset: '-=775',
-      delay: function(el, i) {
-        return 34 * (i+1)
-      }
-    }).add({
-      targets: '.ml11',
-      opacity: 0,
-      duration: 1000,
-      easing: "easeOutExpo",
-      delay: 1000
+    $('.ml11 .letters').each(function () {
+        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
     });
+
+    anime.timeline({ loop: true })
+        .add({
+            targets: '.ml11 .line',
+            scaleY: [0, 1],
+            opacity: [0.5, 1],
+            easing: "easeOutExpo",
+            duration: 1200
+        })
+        .add({
+            targets: '.ml11 .line',
+            translateX: [0, $(".ml11 .letters").width()],
+            easing: "easeOutExpo",
+            duration: 600,
+            delay: 100
+        }).add({
+            targets: '.ml11 .letter',
+            opacity: [0, 1],
+            easing: "easeOutExpo",
+            duration: 1200,
+            offset: '-=775',
+            delay: function (el, i) {
+                return 34 * (i + 1)
+            }
+        }).add({
+            targets: '.ml11',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
 });
