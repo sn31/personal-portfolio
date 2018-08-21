6b030dd8c61b1f6ed8653762fb8d67447942ebd0
SkyeN <skye@dames.es>
2018-08-20 20:22:47 -0700

    add splash screen heading

--- js/scripts.js
+++ js/scripts.js
@@ -6,33 +6,4 @@ $(document).ready(function () {
     $('.enter_link').click(function () {
         $("#splashscreen").fadeOut(500);
     });
-    anime.timeline({loop: true})
-    .add({
-      targets: '.ml5 .line',
-      opacity: [0.5,1],
-      scaleX: [0, 1],
-      easing: "easeInOutExpo",
-      duration: 700
-    }).add({
-      targets: '.ml5 .line',
-      duration: 600,
-      easing: "easeOutExpo",
-      translateY: function(e, i, l) {
-        var offset = -0.625 + 0.625*2*i;
-        return offset + "em";
-      }
-    }).add({
-      targets: '.ml5 .letters',
-      opacity: [0,1],
-      translateX: ["0.5em", 0],
-      easing: "easeOutExpo",
-      duration: 600,
-      offset: '-=300'
-    }).add({
-      targets: '.ml5',
-      opacity: 0,
-      duration: 1000,
-      easing: "easeOutExpo",
-      delay: 1000
-    });
-})
+});
