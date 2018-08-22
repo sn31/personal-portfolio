76200b517ec7ae5382d2080ef10cb3ee12afb31c
SkyeN <skye@dames.es>
2018-08-21 21:38:11 -0700

    add autoplay to carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-   
+    $('#myCarousel').carousel({
+        interval: 3000,
+        cycle: true
+      }); 
     $('.enter_link').click(function () {
         $("#splashscreen").fadeOut(500);
     });
