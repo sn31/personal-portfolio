47145f010e88db842d1865e3ce6565824f631a50
SkyeN <skye@dames.es>
2018-11-10 11:54:19 -0800

    fix moving letters loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,100 @@
 var cardPos = {
-    "about-me": $(window).height(),
-    "projects": 2 * $(window).height(),
-    "background": 3 * $(window).height(),
-    "hobbies": 4 * $(window).height()-1,
-}
+  "about-me": $(window).height(),
+  projects: 2 * $(window).height(),
+  background: 3 * $(window).height(),
+  hobbies: 4 * $(window).height() - 1
+};
 
-$(document).ready(function () {
-    // getLocation();
+$(document).ready(function() {
+  // getLocation();
 
-    console.log($('html').offset().top);
-    $('#myCarousel').carousel({
-        interval: 3000,
-        cycle: true
+  console.log($("html").offset().top);
+  $("#myCarousel").carousel({
+    interval: 3000,
+    cycle: true
+  });
+  $(".enter_link").click(function() {
+    $("#splashscreen").fadeOut(500);
+  });
+
+  // Wrap every letter in a span
+  $(".ml11 .letters").each(function() {
+    $(this).html(
+      $(this)
+        .text()
+        .replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
+    );
+  });
+
+  anime
+    .timeline({ loop: true })
+    .add({
+      targets: ".ml11 .line",
+      scaleY: [0, 1],
+      opacity: [0.5, 1],
+      easing: "easeOutExpo",
+      duration: 700
+    })
+    .add({
+      targets: ".ml11 .line",
+      translateX: [0, $(".ml11 .letters").width()+170],
+      easing: "easeOutExpo",
+      duration: 700,
+      delay: 100
+    })
+    .add({
+      targets: ".ml11 .letter",
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: "-=775",
+      delay: function(el, i) {
+        return 34 * (i + 1);
+      }
+    })
+    .add({
+      targets: ".ml11",
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
     });
-    $('.enter_link').click(function () {
-        $("#splashscreen").fadeOut(500);
-    });
+  // Fix navbar at the top of every section
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > $(window).height()) {
+      $(".pageNav").addClass("pageNavTest");
+      $(".pageNav").removeClass("pageNav");
+      $(".pageNavTest").slideDown();
+    } else if ($(window).scrollTop() < $(window).height()) {
+      $(".pageNavTest").hide();
+    }
 
-    // Wrap every letter in a span
-    $('.ml11 .letters').each(function () {
-        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-    });
-
-    anime.timeline({ loop: true })
-        .add({
-            targets: '.ml11 .line',
-            scaleY: [0, 1],
-            opacity: [0.5, 1],
-            easing: "easeOutExpo",
-            duration: 1200
-        })
-        .add({
-            targets: '.ml11 .line',
-            translateX: [0, $(".text-wrapper").width()],
-            easing: "easeOutExpo",
-            duration: 580,
-            delay: 80
-        }).add({
-            targets: '.ml11 .letter',
-            opacity: [0, 1],
-            easing: "easeOutExpo",
-            duration: 1200,
-            offset: '-=775',
-            delay: function (el, i) {
-                return 34 * (i + 1)
-            }
-        }).add({
-            targets: '.ml11',
-            opacity: 0,
-            duration: 1000,
-            easing: "easeOutExpo",
-            delay: 1000
+    //Change url without reloading the page
+    var scrollPos = $(document).scrollTop();
+    for (var key in cardPos) {
+      if (Math.abs(cardPos[key] - scrollPos) < 3) {
+        console.log(key);
+        history.pushState(
+          {},
+          "",
+          "file:///Users/skye/Documents/personal-portfolio/index.html#" + key
+        );
+        $("#movingNav a").each(function() {
+          if ($(this).prop("href") == window.location.href) {
+            $(this).addClass("current");
+          }
         });
+      }
+    }
+  });
 
-    // Fix navbar at the top of every section
-    $(window).scroll(function () {
+  // $(function () {
+  //     $('#movingNav a').each(function () {
+  //         if ($(this).prop('href') == window.location.href) {
+  //             $(this).addClass('current');
+  //         }
 
-        if ($(window).scrollTop() > $(window).height()) {
-            $('.pageNav').addClass("pageNavTest");
-            $('.pageNav').removeClass("pageNav");
-            $('.pageNavTest').slideDown();
-        }
-        else if ($(window).scrollTop() < $(window).height()) {
-            $(".pageNavTest").hide();
-        }
+  //     });
 
-        //Change url without reloading the page
-        var scrollPos = $(document).scrollTop();
-        for (var key in cardPos) {
-            if (Math.abs(cardPos[key] - scrollPos) < 3) {
-                console.log(key);
-                history.pushState({}, '', 'file:///Users/skye/Documents/personal-portfolio/index.html#' + key);
-                $('#movingNav a').each(function () {
-                    if ($(this).prop('href') == window.location.href) {
-                        $(this).addClass('current');
-                    }
-        
-                });
-            }
-        }
-    });
-    
-
-    // $(function () {
-    //     $('#movingNav a').each(function () {
-    //         if ($(this).prop('href') == window.location.href) {
-    //             $(this).addClass('current');
-    //         }
-
-    //     });
-
-    // });
+  // });
 });
